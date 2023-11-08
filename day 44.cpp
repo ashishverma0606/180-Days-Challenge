48a5fc5a968f80a2d6044b43421598bcdf4235b8
Ashish <verma.632003@gmail.com>
2023-11-08 13:34:33 +0530

    Add files via upload

---
+++ day 44.cpp
@@ -0,0 +1,74 @@
+
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
+
+
+
+// void wave(int arr[][4], int row, int col)
+// {
+//     for(int j=0; j<col; j++)
+//     {
+//         if(j%2 == 0)
+//         {
+//             for(int i=0; i<row; i++)
+//             cout<<arr[i][j]<<" ";
+//         }
+//         else
+//         {
+//              for(int i=row-1; i>=0; i--)
+//             cout<<arr[i][j]<<" ";
+//         }
+//     }
+// }
+
+
+
+
+// int main()
+// {
+    // Create 2d vector
+    
+    // int n,m;
+    // cout<<"Enter rows and col: ";
+    // cin>>n>>m;
+
+
+
+    // vector<vector<int> >matrix(n, vector<int>(m,1));
+    
+    // for(int i=0; i<3; i++)
+    // for(int j=0; j<4; j++)
+    // cout<<matrix[i][j]<<" ";
+    // return 0;
+
+
+
+    // cout<<"Rows "<<matrix.size();
+    // cout<<endl;
+    // cout<<"Columns "<<matrix[0].size();
+
+
+
+        // for(int i=0; i<n; i++)
+        // for(int j=0; j<m; j++)
+        // cin>>matrix[i][j];
+
+
+
+    // for(int i=0; i<n; i++)
+    // {
+    //     for(int j=0; j<m; j++)
+    //     cout<<matrix[i][j]<<" ";
+    //     cout<<endl;
+    // }
+    
+    
+    
+    
+    // wave form
+    // int arr[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    // wave(arr, 3, 4);
+    
+}
